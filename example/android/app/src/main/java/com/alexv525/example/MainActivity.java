d024431341fb45f45e3825042e57904245b6366e
Alex <github@alexv525.com>
2020-12-19 23:56:52 +0800

    :recycle: Migrate example to AndroidX and embedding v2.

--- example/android/app/src/main/java/com/alexv525/example/MainActivity.java
+++ example/android/app/src/main/java/com/alexv525/example/MainActivity.java
@@ -1,13 +1,6 @@
 package com.alexv525.example;
 
-import android.os.Bundle;
-import io.flutter.app.FlutterActivity;
-import io.flutter.plugins.GeneratedPluginRegistrant;
+import io.flutter.embedding.android.FlutterActivity;
 
 public class MainActivity extends FlutterActivity {
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    GeneratedPluginRegistrant.registerWith(this);
-  }
 }
